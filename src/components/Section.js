79092604033127e38519dc8cc340ce606e2536c0
Sundara Ram Matta <msram0210@gmail.com>
2021-09-27 21:12:11 -0400

    images for the application

---
+++ src/components/Section.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Section() {
+  return (
+    <Wrap>
+      Section
+    </Wrap>
+  )
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: orange;
+  background-image: url('/images/model-s.jpg');
+`
