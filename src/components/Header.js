43f4efd4edb4d206edc47f635d0f03cb63912b2f
Sundara Ram Matta <msram0210@gmail.com>
2021-09-28 22:04:36 -0400

    adding the logo and styling the overall header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <div>
-      header
-    </div>
+    <Container>
+      <a>
+        <img src="./images/logo.svg" alt="tesla_logo"/>
+      </a>
+      <Menu>
+
+      </Menu>
+    </Container>
   )
 }
 
 export default Header;
+
+// Header has the Logo
+const Container = styled.div`
+  min-height: 3.5rem;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 1.5rem;
+`;
+
+const Menu = styled.div`
+
+`;
